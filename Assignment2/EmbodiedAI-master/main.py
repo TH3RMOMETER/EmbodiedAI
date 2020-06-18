9ab82c250d1e8d2747c8c5402522007de6e1a3e0
TH3RMOMETER <46961065+TH3RMOMETER@users.noreply.github.com>
2020-06-18 13:47:20 +0200

    Add files via upload

---
+++ Assignment2/EmbodiedAI-master/main.py
@@ -0,0 +1,16 @@
+from simulation.simulation import Simulation
+import pygame
+
+#from experiments.flocking import parameters as p
+# from experiments.aggregation import parameters as p
+from experiments.covid import parameters as p
+
+"""
+Code for multi-agent simulation in PyGame with/without physical objects in the environment
+"""
+
+if __name__ == "__main__":
+    pygame.init()
+    sim = Simulation(num_agents=p.N_AGENTS, screen_size=p.SCREEN,
+                     swarm_type=p.SWARM, iterations=p.FRAMES)
+    sim.run()
